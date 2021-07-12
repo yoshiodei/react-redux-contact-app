06280e662c7b0b1ccc15e4349cb7e27fe3e1eb28
yoshiodei <yoshiodei@yahoo.com>
2021-07-12 08:49:16 +0000

    last commit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ const handleAdd = (newContact) => {
 
   return (
     <div className="App">
+              <h2>Yoshi's Contact List</h2>
               <ContactsForm handleAdd={handleAdd}/>
               <Contacts contacts={contacts} /> 
     </div>
--- src/Contacts.jsx
+++ src/Contacts.jsx
@@ -4,7 +4,7 @@ const Contacts = ({ contacts }) => {
     return(
         contacts.map((contact, index)=>{
             return(
-                <div key={index}>
+                <div className="contact__list" key={index}>
                     <p>Name: {contact.name}</p>
                     <p>Phone: {contact.pnum}</p>
                     <p>Location: {contact.loc}</p>
