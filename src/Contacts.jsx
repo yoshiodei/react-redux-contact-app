3956af9206906c5a7baccdb10cf0eccfd55bffb9
yoshiodei <yoshiodei@yahoo.com>
2021-07-12 08:24:17 +0000

    add contacts component

---
+++ src/Contacts.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contacts = ({ contacts }) => {
+    return(
+        contacts.map((contact, index)=>{
+            return(
+                <div key={index}>
+                    <p>Name: {contact.name}</p>
+                    <p>Phone: {contact.pnum}</p>
+                    <p>Location: {contact.loc}</p>
+                    <hr />
+                </div>
+            )
+        
+        })
+    )
+        
+}
+
+export default Contacts;
