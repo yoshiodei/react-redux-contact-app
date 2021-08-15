531355a2970273f6cf0f4e70f53c6d2d36e482f2
yoshiodei <yoshiodei@yahoo.com>
2021-08-15 22:49:25 +0000

    add reducer

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,17 @@
+let initialState = [{
+                     name: 'Yoshi Odei',
+                     phone: '12345',
+                     location: 'Accra' 
+                   },
+                   {
+                    name: 'Leo Messi',
+                    phone: '6789',
+                    location: 'Paris' 
+                  },
+                ]
+
+const contactReducer = (state = initialState, action) => {
+    
+}
+
+export default contactReducer;
